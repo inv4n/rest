83198886200544948d4d3b69eb4f21930f779e2b
inv4n <42974205+inv4n@users.noreply.github.com>
2020-08-03 11:03:30 -0500

    Add files via upload

---
+++ springboot/src/main/java/com/javarevolutions/springboot/service/RestUser.java
@@ -0,0 +1,36 @@
+package com.javarevolutions.springboot.service;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javarevolutions.springboot.vo.VOUser;
+
+@RestController
+@RequestMapping(path = "/servicesREST/JR")
+public class RestUser {
+
+	@RequestMapping(
+			method = RequestMethod.POST,
+			path = "/validateUser",
+			consumes = "application/json",
+			produces = "application/json")
+	public @ResponseBody VOUser validateUser(@RequestBody VOUser user) throws Exception {
+		VOUser response = new VOUser();
+		response.setFind(false);
+		System.out.println("User: " + user.getUser());
+		System.out.println("Password: " + user.getPassword());
+		try {
+			if (user.getUser().equals("Java") && user.getPassword().equals("Revolutions")) {
+				response.setUser(user.getUser());
+				response.setFind(true);
+			}
+			return response;
+		} catch (Exception ex) {
+
+			return response;
+		}
+	}
+}
